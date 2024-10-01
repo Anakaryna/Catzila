1f8b0a941549eb1c588bb267f84335db23ad40fc
Frisk <bonnerondorian@gmail.com>
2024-10-01 15:31:37 +0200

    we have feedback

--- mine_turtle.gd
+++ mine_turtle.gd
@@ -4,13 +4,14 @@ const HELLO_MINE_TURTLE = preload("res://assets/homeMade/hello mine turtle.mp3")
 @onready var player: CharacterBody2D = %player
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var explosion_area: Area2D = $explosionArea
+@onready var sprite: AnimatedSprite2D = $Sprite
 
 var destination: Vector2 = Vector2(150, 200)
 var movementSpeed = 170
 var fused = false
 var exploded = false
 var fuseStarted: float = 0
-var fuseDuration: float = 1500
+var fuseDuration: float = 1900
 var audioDuration: float = 2840
 var explodedBodies = Array()
 
@@ -27,6 +28,7 @@ func _process(delta: float) -> void:
 		#print("im on scene")
 		self.get_overlapping_bodies().any(collisionEnter)
 	if fused and not exploded and Time.get_ticks_msec() >= fuseStarted + fuseDuration:
+		exploded = true
 		explosion_area.get_overlapping_bodies().all(sendDamage)
 		pass
 	if fused and Time.get_ticks_msec() >= fuseStarted + audioDuration:
@@ -36,6 +38,8 @@ func _process(delta: float) -> void:
 
 func collisionEnter(body):
 	if body == player and not fused:
+		sprite.play(&"Hello")
+		sprite.animation_finished.connect(helloFinished)
 		if not audio_stream_player_2d.playing:
 			audio_stream_player_2d.stream = HELLO_MINE_TURTLE
 			audio_stream_player_2d.play()
@@ -43,6 +47,12 @@ func collisionEnter(body):
 		fused = true
 		fuseStarted = Time.get_ticks_msec()
 	pass
+
+func helloFinished():
+	sprite.play(&"Explosion")
+	sprite.animation_finished.disconnect(helloFinished)
+	pass
+
 
 func sendDamage(body):
 	if not explodedBodies.has(body):
