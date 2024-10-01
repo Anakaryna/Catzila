cc087190cf6fc8c9b2fbe3c91f772e33e54f1b27
Chachoune963 <sacha@toutut.fr>
2024-10-01 17:10:04 +0200

    Basic Cat AI and animations

---
+++ scripts/entities/laser_cat.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+const SPEED = 90
+
+@onready var laserModule: LaserShooter = $LaserShooterModule
+@onready var texture: AnimatedSprite2D = $LaserCatTexture
+
+var target: Node2D = null
+var anim_base: String = "idle"
+var anim_direction: String = "down"
+
+func _physics_process(delta: float) -> void:
+	var input = get_movement_direction()
+	
+	velocity = input.normalized() * SPEED
+	
+	animate()
+	move_and_slide()
+
+# TODO: Put AI logic in this function to determine movement 
+func get_movement_direction() -> Vector2:
+	if (target):
+		return position - target.position
+		
+	return Vector2.ZERO
+
+func animate():
+	if (target):
+		# Update direction
+		anim_base = "attack"
+		var angle = get_angle_to(target.position)
+		if (-PI / 4 <= angle && angle < PI / 4):
+			anim_direction = "right"
+		if (PI / 4 <= angle && angle < 3 * PI / 4):
+			anim_direction = "down"
+		if (3 * PI / 4 <= angle || angle < - 3 * PI / 4):
+			anim_direction = "left"
+		if (- 3 * PI / 4 <= angle && angle < - PI / 4):
+			anim_direction = "up"
+	else:
+		anim_base = "idle"
+	
+	print(target)
+	texture.animation = anim_base + "_" + anim_direction
+
+func _on_target_detection(body: Node2D) -> void:
+	target = body
+	laserModule.target = target
+
+
+func _on_target_exited(body: Node2D) -> void:
+	target = null
