05a4fd8b4a06ce6311aa03536f12bb863fd7e522
Frisk <bonnerondorian@gmail.com>
2024-10-02 11:55:36 +0200

    50/50

--- Scripts/sbire.gd
+++ Scripts/sbire.gd
@@ -3,11 +3,22 @@ extends CharacterBody2D
 class_name Sbire
 
 @export var damage: float = 20
+var health = 100
+
+func _ready() -> void:
+	EventBus.signal_damage.connect(_on_signal_damage)
 
 func _physics_process(delta):
 	move_and_slide()
 	
 
+func _process(delta: float) -> void:
+	if health <= 0:
+		queue_free()
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
 	EventBus.signal_damage.emit(body, damage)
+
+func _on_signal_damage(body: CharacterBody2D, damagePoints: float):
+	if body == self:
+		health -= damagePoints
