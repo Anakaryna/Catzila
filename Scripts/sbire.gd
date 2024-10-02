013af86af9e03123c6bc87834ec8ebb8c1c5abcd
Anakaryna <78374876+Anakaryna@users.noreply.github.com>
2024-10-02 11:04:36 +0200

    Add Sbire attack

--- Scripts/sbire.gd
+++ Scripts/sbire.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 class_name Sbire
 
+@export var damage: float = 20
+
 func _physics_process(delta):
 	move_and_slide()
 	
+
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	EventBus.signal_damage.emit(body, damage)
